bfdad99c5ec0bde1c15dd3fa9e4f39ef50c3c64a
Pablo Barrera <pabloandresbarrera0@gmail.com>
2024-03-08 23:42:35 -0500

    creacion del proyecto

--- src/main/java/edu/unimagdalena/modelo_relacional/repositorios/MensajeRepository.java
+++
@@ -1,7 +0,0 @@
-package edu.unimagdalena.modelo_relacional.repositorios;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import edu.unimagdalena.modelo_relacional.entidades.Mensaje;
-
-public interface MensajeRepository extends JpaRepository<Mensaje,Long>{}
--- src/main/java/edu/unimagdalena/modelo_relacional/repositorios/SugerenciaRepository.java
+++
@@ -1,7 +0,0 @@
-package edu.unimagdalena.modelo_relacional.repositorios;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import edu.unimagdalena.modelo_relacional.entidades.Sugerencia;
-
-public interface SugerenciaRepository extends JpaRepository<Sugerencia,Long>{}
