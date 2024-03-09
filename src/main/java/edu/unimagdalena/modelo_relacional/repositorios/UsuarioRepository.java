a7b01233fbc7f25eb7a1d0e59b0ad524c09bab2a
Pablo Barrera <pabloandresbarrera0@gmail.com>
2024-03-08 23:37:52 -0500

    creacion del modelo relacional

--- src/main/java/edu/unimagdalena/modelo_relacional/repositorios/UsuarioRepository.java
+++
@@ -1,7 +0,0 @@
-package edu.unimagdalena.modelo_relacional.repositorios;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import edu.unimagdalena.modelo_relacional.entidades.Usuario;
-
-public interface UsuarioRepository extends JpaRepository<Usuario,Long>{}
